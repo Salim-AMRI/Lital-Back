eed7cc34128719f3d7eca120e55e1c3d46e473ad
Salim-Simplon <amri.salim.simplon@gmail.com>
2020-11-23 21:50:24 +0100

    Second Version

--- control/users_control.js
+++ control/users_control.js
@@ -1,14 +1,16 @@
 const User_model = require("../model/users_model");
+const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// const config = require("config");
+
 
 
 module.exports = {
 
     get_all: async (req, res, next) => {
-        
-        
         User_model
             .find()
-            .then(x => {res.json(x);console.log("get all",x);})
+            .then(x => res.send(x))
             .catch(r => res.send(r.message))
 
     },
@@ -24,11 +26,12 @@ module.exports = {
     },
 
     post_one: async (req, res, next) => {
-        
+        const hashed_password = await bcrypt.hash(req.body.passWord, 10);
         new User_model({
             userName: req.body.userName,
             userMail: req.body.userMail,
-            passWord: req.body.passWord,
+            passWord: hashed_password,
+            //passWord: req.body.passWord,
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             userPhone: req.body.userPhone,
@@ -39,6 +42,21 @@ module.exports = {
             .catch(r => res.send(r.message))
 
 
+
+    },
+
+    log_one: async (req, res, next) => {
+
+        let logger = await User_model.findOne({ userName: req.body.userName });
+        if (!logger) return res.status(400).send("Email not found");
+
+        const pass = await bcrypt.compare(req.body.passWord, logger.passWord);
+        if (!pass) return res.status(400).send("Password did not match");
+
+        const token = logger.token_gen()
+
+        res.header("x-auth-token", token).send(logger)
+        // res.send("logger penetrated successfully, token :", token)
 
     },
 
--- route/users_route.js
+++ route/users_route.js
@@ -1,17 +1,20 @@
-
+const auth = require("../middlewares/auth");
+const perm = require("../middlewares/perm");
 const express = require("express");
 const router = express.Router();
 const users_control = require("../control/users_control");
 
 
-router.get("/", users_control.get_all);
-router.get("/:id", users_control.get_one_by_id);
+router.get("/", [auth, perm("Administrateur")], users_control.get_all);
+router.get("/:id", [auth, perm("Administrateur")], users_control.get_one_by_id);
 
 router.post("/", users_control.post_one);
 
-router.put("/:id", users_control.put_one_by_id);
+router.post("/log", users_control.log_one);
 
-router.delete("/:id", users_control.delete_one_by_id);
+router.put("/:id", [auth, perm("Administrateur")], users_control.put_one_by_id);
+
+router.delete("/:id", [auth, perm("Administrateur")], users_control.delete_one_by_id);
 
 
 module.exports = router;
